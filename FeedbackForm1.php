63a5bd1fb9a95b8da83fc3892afb14c20c52f048
Lim-YongHau <66451328+Lim-YongHau@users.noreply.github.com>
2020-06-22 11:42:26 +0800

    Switch

---
+++ FeedbackForm1.php
@@ -0,0 +1,257 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database ="loginntd";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST['insert'])){
+    $customerID=$_POST['customerID']; //received input value
+    $customerEmail=$_POST['customerEmail'];
+    $customerPhone=$_POST['customerPhone'];
+    $customerMessage=$_POST['customerMessage'];
+  
+    
+    echo $sql="insert into feedback values('$customerID','$customerEmail','$customerPhone','$customerMessage')";
+    
+    $result=$conn->query($sql);
+    
+    }
+    
+    if (isset($_GET['id'])){
+        $id=$_GET['id']; 
+        $sql="select * from feedback where id='$id'";
+        $result=$conn->query($sql);
+        if($result->num_rows>0){
+            while($row=$result->fetch_assoc()){
+        //display result
+         $id=$row['id'];
+         $email=$row['email'];
+         $phone=$row['phone'];
+         $address=$row['message'];
+         
+    
+    }
+        }
+    }
+    
+    if(isset($_POST['update'])){
+    $customerID=$_POST['customerID'];
+    $customerEmail=$_POST['customerEmail'];  
+    $customerPhone=$_POST['customerPhone'];
+    $customerMessage=$_POST['customerMessage'];
+  
+    
+    $sql="update feedback set email='$customerEmail',phone='$customerPhone',message='$customerMessage' where id='$customerID'";
+    $result=$conn->query($sql);
+    echo "<script> window.location.assign('ViewFeedback.php'); </script>";
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <style>
+ body{
+  margin: 0;
+  padding: 0;
+  background: url(background1.png);
+  background-size: cover;
+}
+.contact-form{
+  width: 85%;
+  max-width: 600px;
+  background: #f1f1f1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  padding: 30px 40px;
+  box-sizing: border-box;
+  border-radius: 8px;
+  text-align: center;
+  box-shadow: 0 0 20px #000000b3;
+  font-family: "Montserrat",sans-serif;
+}
+.contact-form h1{
+  margin-top: 0;
+  font-weight: 200;
+}
+.txtb{
+  border:1px solid gray;
+  margin: 8px 0;
+  padding: 12px 18px;
+  border-radius: 8px;
+}
+.txtb label{
+  display: block;
+  text-align: left;
+  color: #333;
+  text-transform: uppercase;
+  font-size: 14px;
+}
+.txtb input,.txtb textarea{
+  width: 100%;
+  border: none;
+  background: none;
+  outline: none;
+  font-size: 18px;
+  margin-top: 6px;
+}
+.btn{
+  display: inline-block;
+  padding: 14px 0;
+  text-transform: uppercase;
+  cursor: pointer;
+  margin-top: 8px;
+  width: 100%;
+}
+</style>
+
+<head>
+<meta charset="utf-8">
+     <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title></title>
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark  bg-danger">
+    <nav class="navbar navbar-expand-lg navbar-dark  bg-danger">
+      <a class="navbar-brand" href="home.php">
+        <img src="logo.png" class="img-fluid rounded-circle" width="45" height="50" alt="">
+        Game Time
+      </a>
+    </nav>
+   
+    
+    <button class="navbar-toggler mb-0 h2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link mb-0 h5" href="home.php">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  mb-0 h5" href="SelectionClothes.php">My Store<span class="badge badge-pill badge-success">3</span>
+            <img src="sopping.png" class="img-fluid rounded-circle" width="25" height="25" alt="">
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link  mb-0 h5 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Help
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="FeedbackForm1.php">Feedback Form and Massage</a>
+              <a class="dropdown-item" href="CustomerDetail.php">Sign up</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="videoPage.php">Video n News</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  mb-0 h5" href="b.php" tabindex="-1" aria-disabled="true">Log in</a>
+        </li>
+      </ul>
+      
+    </div>
+  </nav>
+<!------ Include the above in your HEAD tag ---------->
+
+
+
+<body>
+
+<div class="contact-form">
+                    <h3 class="text-center text-info">
+                            <?php
+	                  if(isset($_GET['id'])){
+	                    	echo"Update Form";
+
+	                        }else{
+	                     	echo"Feedback Form";
+                      	}
+	                   ?></h3>
+
+                        <form clss="contact-form" class="" action="" method="post">
+                         
+                         
+                            
+                            <div class="txtb">
+                                <label for="customerID" class="text-info">Full Name :</label>
+                                <input type="text" name="customerID" id="customerID" class="form-control" placeholder="Enter Your Name" value ="<?php if(isset($_GET['id'])){echo  $id;}?>">
+                            </div>
+
+                            <div class="txtb">
+                                <label for="customerEmail" class="text-info">Email:</label>
+                                <input type="text" name="customerEmail" id="customerEmail" class="form-control" placeholder="Enter Your Email"value ="<?php if(isset($_GET['id'])){echo  $email;}?>">
+                            </div>
+
+                            <div class="txtb">
+                                <label for="customerPhone" class="text-info">Phone:</label>
+                                <input type="text" name="customerPhone" id="customerPhone" class="form-control" placeholder="Enter Your Phone Number" value ="<?php if(isset($_GET['id'])){echo  $phone;}?>">
+                            </div>
+
+                            <div class="txtb">
+                                <label for="customerMessage" class="text-info">Message :</label>
+                                <input type="text" name="customerMessage" id="customerMessage" class="form-control" value ="<?php if(isset($_GET['id'])){echo  $address;}?>">
+                                <textarea></textarea>
+                            </div>
+
+                            
+                        
+                           
+	                      <?php
+                            if(isset($_GET['id'])){
+		                     echo'<button type="submit" class="btn" name="update">Update Customer </botton>';
+	
+	                        }else{
+	                    	 echo'<button type="submit" href="a.php" class="btn" style="color:red;background:yellow;" name="insert" ><h5>SEND</h5></botton>';
+	                         }
+	                        ?>
+	
+
+	                     </div> <!-- form-group// -->      
+                        </form>
+
+                        <?php
+	                  if(isset($_GET['id'])){
+                            echo' 
+                            <style>
+                              body  {
+                                background-image: url("background3.jpg");
+                               }<style>';
+
+	                        }else{
+	                     	echo ' <style>
+                              body  {
+                               background-image: url("background1.png");
+                              }<style>';
+                      	}
+	                   ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,255 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>Game Time</title>
+  <style>
+ .icon-bar {
+  position: fixed;
+  top: 35%;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+.icon-bar a {
+  display: block;
+  text-align: center;
+  padding: 10px;
+  transition: all 0.3s ease;
+  color:yellow;
+  font-size: 20px;
+}
+
+.icon-bar a:hover {
+  background-color: #000;
+}
+
+.facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.twitter {
+  background: #55ACEE;
+  color:yellow;
+}
+
+.google {
+  background: #dd4b39;
+  color: white;
+}
+
+    
+  </style>
+</head>
+
+<body>
+   
+
+  <nav class="navbar navbar-expand-lg navbar-dark  bg-danger">
+    <nav class="navbar navbar-expand-lg navbar-dark  bg-danger">
+      <a class="navbar-brand" href="home.php">
+        <img src="logo.png" class="img-fluid rounded-circle" width="45" height="50" alt="">
+        Game Time
+      </a>
+    </nav>
+   
+    
+    <button class="navbar-toggler mb-0 h2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link mb-0 h5" href="home.php">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  mb-0 h5" href="  SelectionClothes.php">My Store<span class="badge badge-pill badge-success">3</span>
+            <img src="sopping.png" class="img-fluid rounded-circle" width="25" height="25" alt="">
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link  mb-0 h5 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Help
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="FeedbackForm1.php">Feedback Form and Massage</a>
+              <a class="dropdown-item" href="CustomerDetail.php">Sign up</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="videoPage.php">Video n News</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  mb-0 h5" href="b.php" tabindex="-1" aria-disabled="true">Log in</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+
+  <!---<div class="row">
+            <div class="col-sm-3" style="background-color:red">.col-sm-4</div>
+            <div class="col-sm-2" style="background-color:blue">.col-sm-2</div>
+            <div class="col-sm-2" style="background-color:yellow">.col-sm-2</div>
+            <div class="col-sm-2" style="background-color:purple">.col-sm-2</div>
+            <div class="col-sm-3" style="background-color:saddlebrown">.col-sm-3</div>
+    </div>--->
+
+
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="row">
+          <div class="col-12">
+            <img src="GameTime.jpg" class="d-block w-100 h-100" alt="...">
+          </div>
+        </div>
+      </div>
+       <div class="carousel-item">
+                    <img src="title (1).jpg" class="d-block w-100 " alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="title (2).jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="title (3).jpg" class="d-block  w-100 " alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="title (4).jpg" class="d-block w-100 " alt="...">
+                  </div> 
+
+
+
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+
+  </div>
+
+  <div class="container">
+    <div class="card-group">
+      <div class="card">
+        <img src="1.gif" class="card-img-top" alt="...">
+        <div class="card-body">
+         
+        </div>
+      </div>
+      <div class="card">
+        <img src="3.gif" class="card-img-top" alt="...">
+        <div class="card-body">
+         
+        </div>
+      </div>
+      <div class="card">
+        <img src="2.gif" class="card-img-top" alt="...">
+        <div class="card-body">
+         
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+</body>
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+  <div class="row">
+    <div class="col-4 col-md">
+      <img class="mb-2" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+      <small class="d-block mb-3 text-muted">My Clothes © 2019</small>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Contact us</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="https://web.facebook.com/?_rdc=1&_rdr">Facebook</a></li>
+        <li><a class="text-muted" href="https://www.instagram.com/">Instagram</a></li>
+        <li><a class="text-muted" href="https://twitter.com/login">Twitter</a></li>
+        <li><a class="text-muted" href="https://facebook.com/yongsiang.briansee">contact no:016-7565998</a></li>
+
+      </ul>
+    </div>
+    <div class="col-4 col-md" data-spm-anchor-id="a2o4k.13056827.3838113200.i1.6ec1445drnMQOs">
+      <h4 class="lzd-footer-h4">Delivery Services</h4>
+      
+        
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB1T_EMSkPoK1RjSZKbXXX1IXXa-311-118.png" alt="lel" height="40">   
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB1BMIzSgTqK1RjSZPhXXXfOFXa-294-46.png" alt="nanji" height="15">
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB1.lALSa6qK1RjSZFmXXX0PFXa-78-30.png" alt="gdex" height="15">
+        
+      </br>
+        
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB1GQ3fk_Zmx1VjSZFGXXax2XXa-108-30.png" alt="laju" height="15">
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB1D4kVShYaK1RjSZFnXXa80pXa-310-85.png" alt="sky" height="20">
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB18UEASgDqK1RjSZSyXXaxEVXa-846-409.png" alt="lorry" height="20">
+        
+      </br>
+        
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB1gN35SiLaK1RjSZFxXXamPFXa-578-230.png" alt="century" height="30">
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB1VQoJSkzoK1RjSZFlXXai4VXa-400-200.png" alt="abx" height="30">
+        <img class="lzd-icon-delivery pull-left " src="https://laz-img-cdn.alicdn.com/tfs/TB1y3IzSgTqK1RjSZPhXXXfOFXa-447-123.png" alt="j&amp;t" height="30" data-spm-anchor-id="a2o4k.13056827.3838113200.i2.6ec1445drnMQOs">
+        
+    </div>  
+
+   
+    <div class="col-6 col-md">
+      <h5>Payment Methods</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="text-muted" href="#">CIMB </a></li>
+        <li><a class="text-muted" href="#">Maybank</a></li>
+        <li><a class="text-muted" href="#">Hong Leong</a></li>
+        <li><a class="text-muted" href="#">RHB</a></li>
+        <li><a class="text-muted" href="#">Visa</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
+<div class="icon-bar">
+    <a href="https://web.facebook.com/?_rdc=1&_rdr" class="facebook"><i class="fa fa-facebook"style="margin:0px"><img src="facebook.png" width="30"></i></a> 
+    <a href="https://twitter.com/login" class="twitter"><i class="fa fa-twitter"style="margin:0px"><img src="twitter.jpg" width="30"></i></a> 
+    <a href="https://www.google.com/?hl=ms" class="google"><i class="fa fa-google"style="margin:0px"><img src="google.png" width="30"></i></a> 
+   
+  </div>
+
+</html>
